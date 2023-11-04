c2d95cd8aedebdc190f8500a0250dc4c743b10bc
Monstrous <49096535+monstroussoftware@users.noreply.github.com>
2023-11-04 17:11:44 +0100

    step 2

--- core/src/main/java/com/monstrous/tut3d/CamController.java
+++ core/src/main/java/com/monstrous/tut3d/CamController.java
@@ -1,5 +1,6 @@
 package com.monstrous.tut3d;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.graphics.Camera;
@@ -13,10 +14,17 @@ public class CamController extends InputAdapter {
     public int strafeRightKey = Input.Keys.D;
     public int turnLeftKey = Input.Keys.Q;
     public int turnRightKey = Input.Keys.E;
+    public int jumpKey = Input.Keys.SPACE;
     public int runShiftKey = Input.Keys.SHIFT_LEFT;
 
     protected final Camera camera;
     protected final IntIntMap keys = new IntIntMap();
+    private float bobAngle = 0;
+    private boolean isJumping = false;  // are we jumping?
+    private float jumpH = 0;        // jump height
+    private float jumpV = 0;        // jump velocity
+    private final Vector3 tmp2 = new Vector3();
+    private final Vector3 tmp3 = new Vector3();
 
     public CamController(Camera camera) {
         this.camera = camera;
@@ -36,27 +44,51 @@ public class CamController extends InputAdapter {
 
     public void update (float deltaTime) {
 
+        float bobSpeed= 0;
         float moveSpeed = Settings.walkSpeed;
         if(keys.containsKey(runShiftKey))       // go faster if SHIFT is held down
             moveSpeed *= Settings.runFactor;
 
-        if (keys.containsKey(forwardKey))
+        if (keys.containsKey(forwardKey)) {
             moveForward(deltaTime * moveSpeed);
-
-        if (keys.containsKey(backwardKey))
+            bobSpeed = moveSpeed;
+        }
+        if (keys.containsKey(backwardKey)) {
             moveForward(-deltaTime * moveSpeed);
+            bobSpeed = moveSpeed;
+        }
 
-        if (keys.containsKey(strafeLeftKey))
+        if (keys.containsKey(strafeLeftKey)) {
             strafe(-deltaTime * Settings.walkSpeed);
+            bobSpeed = Settings.walkSpeed;
+        }
 
-        if (keys.containsKey(strafeRightKey))
+        if (keys.containsKey(strafeRightKey)) {
             strafe(deltaTime * Settings.walkSpeed);
+            bobSpeed = Settings.walkSpeed;
+        }
 
         if (keys.containsKey(turnLeftKey))
             rotateView(deltaTime*Settings.turnSpeed );
         else if (keys.containsKey(turnRightKey))
             rotateView(-deltaTime*Settings.turnSpeed );
 
+        if (keys.containsKey(jumpKey) && !isJumping) {
+            isJumping = true;
+            jumpV = 5;                  // initial upward velocity
+        }
+
+        if(isJumping){
+            bobSpeed = 0;               // no head bob while jumping
+            jumpV += deltaTime*Settings.gravity;      // deceleration due to gravity
+            jumpH += jumpV*deltaTime;   // update jump height from velocity
+            if(jumpH < 0){              // if jump height is zero, stop the jump
+                isJumping = false;
+                jumpH = 0;
+            }
+        }
+
+        camera.position.y = Settings.eyeHeight + jumpH + bobHeight( bobSpeed, deltaTime); // apply some head bob if we're moving
 
         camera.update(true);
     }
@@ -82,6 +114,45 @@ public class CamController extends InputAdapter {
     private void rotateView(float deltaX) {
         camera.direction.rotate(camera.up, deltaX);
         camera.up.set(Vector3.Y);
+    }
+
+    private void rotateView(float deltaX, float deltaY) {
+        camera.direction.rotate(camera.up, deltaX);
+
+        // avoid gimbal lock when looking straight up or down
+        Vector3 oldPitchAxis = tmp.set(camera.direction).crs(camera.up).nor();
+        Vector3 newDirection = tmp2.set(camera.direction).rotate(tmp, deltaY);
+        Vector3 newPitchAxis = tmp3.set(tmp2).crs(camera.up);
+        if (!newPitchAxis.hasOppositeDirection(oldPitchAxis))
+            camera.direction.set(newDirection);
+    }
+
+    protected float degreesPerPixel = 0.1f;
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        // ignore big delta jump on start up
+        if(Gdx.input.getDeltaX() == screenX && Gdx.input.getDeltaY() == screenY)
+            return true;
+
+        float deltaX = -Gdx.input.getDeltaX() * degreesPerPixel;
+        float deltaY = -Gdx.input.getDeltaY() * degreesPerPixel;
+        if (Settings.invertLook)
+            deltaY = -deltaY;
+        if (!Settings.freeLook) {    // keep camera movement in the horizontal plane
+            deltaY = 0;
+            camera.direction.y = 0;
+        }
+        rotateView(deltaX, deltaY);
+        return true;
+    }
+
+    private float bobHeight(float speed, float deltaTime ) {
+        if(Math.abs(speed) < 0.1f )
+            return 0f;
+        bobAngle += deltaTime * speed * 0.5f * Math.PI / Settings.headBobDuration;
+        // move the head up and down in a sine wave
+        return (float) (Settings.headBobHeight * Math.sin(bobAngle));
     }
 
 }
--- core/src/main/java/com/monstrous/tut3d/Settings.java
+++ core/src/main/java/com/monstrous/tut3d/Settings.java
@@ -8,5 +8,10 @@ public class Settings {
     static public float runFactor = 3f;    // m/s
     static public float turnSpeed = 120f;   // degrees/s
 
+    static public boolean invertLook = false;
+    static public boolean freeLook = true;
+    static public float headBobDuration = 0.6f; // s
+    static public float headBobHeight = 0.04f;  // m
 
+    static public float gravity = -9.8f; // meters / s^2
 }
