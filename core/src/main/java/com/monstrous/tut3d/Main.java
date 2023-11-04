2a57c2bf9eee258d7ea7de3958eb2ddccf2caca5
Monstrous <49096535+monstroussoftware@users.noreply.github.com>
2023-11-04 14:16:09 +0100

    Initial commit

---
+++ core/src/main/java/com/monstrous/tut3d/Main.java
@@ -0,0 +1,11 @@
+package com.monstrous.tut3d;
+
+import com.badlogic.gdx.Game;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Main extends Game {
+    @Override
+    public void create() {
+        setScreen(new FirstScreen());
+    }
+}
---
+++ teavm/src/main/java/com/monstrous/tut3d/teavm/TeaVMBuilder.java
@@ -0,0 +1,29 @@
+package com.monstrous.tut3d.teavm;
+
+import com.github.xpenatan.gdx.backends.teavm.TeaBuildConfiguration;
+import com.github.xpenatan.gdx.backends.teavm.TeaBuilder;
+import com.github.xpenatan.gdx.backends.teavm.plugins.TeaReflectionSupplier;
+import com.github.xpenatan.gdx.backends.teavm.gen.SkipClass;
+import java.io.File;
+import java.io.IOException;
+import org.teavm.tooling.TeaVMTool;
+
+/** Builds the TeaVM/HTML application. */
+@SkipClass
+public class TeaVMBuilder {
+    public static void main(String[] args) throws IOException {
+        TeaBuildConfiguration teaBuildConfiguration = new TeaBuildConfiguration();
+        teaBuildConfiguration.assetsPath.add(new File("../assets"));
+        teaBuildConfiguration.webappPath = new File("build/dist").getCanonicalPath();
+
+        // Register any extra classpath assets here:
+        // teaBuildConfiguration.additionalAssetsClasspathFiles.add("com/monstrous/tut3d/asset.extension");
+
+        // Register any classes or packages that require reflection here:
+        // TeaReflectionSupplier.addReflectionClass("com.monstrous.tut3d.reflect");
+
+        TeaVMTool tool = TeaBuilder.config(teaBuildConfiguration);
+        tool.setMainClass(TeaVMLauncher.class.getName());
+        TeaBuilder.build(tool);
+    }
+}
